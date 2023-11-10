714f0f9892f8c1873a71ff918a8cc1929525a1ff
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-10 13:03:30 +0200

    Summary

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './js/book-block.js';
+import './js/categories.js';
+import './js/header.js';
 import './js/requests.js';
+import './js/scroll-up.js';
+import './js/book-block.js';
+import './js/sing-in.js';
+import './js/support.js';
 
