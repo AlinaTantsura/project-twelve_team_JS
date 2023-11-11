10a7825f83c5b76f9f9a0a71e356aa3cc21fcf3c
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-11 19:28:21 +0000

    Deploying to gh-pages from @ AlinaTantsura/project-twelve_team_JS@9c99ad0971c09b68c231c2c21b8bb657ca5fed73 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/scroll-up-16d7be60.js";console.log("I am a shopping-list");
+import"./assets/scroll-up-adaa23b2.js";console.log("I am a shopping-list");
 //# sourceMappingURL=commonHelpers2.js.map
