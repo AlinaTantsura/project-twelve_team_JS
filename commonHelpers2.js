590efc26334fd598e20b620732b526a3cedf15c8
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-12 13:49:24 +0000

    Deploying to gh-pages from @ AlinaTantsura/project-twelve_team_JS@c905ad11908c03a5eebc3f7675abeec7d885c0eb 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/support-5ec7a569.js";console.log("I am a shopping-list");const e=document.querySelector(".books-list"),o=document.querySelector(".no-books");e.children.length===0?o.classList.remove("hidden"):o.classList.add("hidden");
+import"./assets/support-0108be30.js";console.log("I am a shopping-list");const e=document.querySelector(".books-list"),o=document.querySelector(".no-books");e.children.length===0?o.classList.remove("hidden"):o.classList.add("hidden");
 //# sourceMappingURL=commonHelpers2.js.map
