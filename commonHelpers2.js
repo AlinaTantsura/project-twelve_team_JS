2e44350340c20bcc22d6c26522e40c296821c24c
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-13 22:38:47 +0000

    Deploying to gh-pages from @ AlinaTantsura/project-twelve_team_JS@1eea56bd794c8cfc0d42f8c5e008c0208420c638 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,7 +1,7 @@
-import"./assets/dark-light-switcher-b7703df8.js";const n=document.querySelector(".books-list");document.querySelector(".delete-form");const a=document.querySelector(".no-books"),d=JSON.parse(localStorage.getItem("shoppingList"));n.insertAdjacentHTML("beforeend",u(d));const g=[...n.children];g.forEach(o=>{o.children[2].addEventListener("click",h)});function h(o){const t=o.currentTarget.dataset.bookId,i=JSON.parse(localStorage.getItem("shoppingList"))||[],s=[];i.filter(e=>{e._id!==t&&s.push(e)}),console.log(s),localStorage.setItem("shoppingList",JSON.stringify(s))}n.children.length===0?a.classList.remove("hidden"):a.classList.add("hidden");function u(o){const t="Description will be added sooner";return o.map(({_id:i,book_image:s,title:e,list_name:c,description:r,author:p,buy_links:l})=>`
+import"./assets/dark-light-switcher-b7703df8.js";const n=document.querySelector(".books-list");document.querySelector(".delete-form");const a=document.querySelector(".no-books"),d=JSON.parse(localStorage.getItem("shoppingList"));n.insertAdjacentHTML("beforeend",u(d));const h=[...n.children];h.forEach(s=>{s.children[2].addEventListener("click",g)});function g(s){const t=s.currentTarget.dataset.bookId,i=JSON.parse(localStorage.getItem("shoppingList"))||[],o=[];i.filter(e=>{e._id!==t&&o.push(e)}),console.log(o),localStorage.setItem("shoppingList",JSON.stringify(o))}n.children.length===0?a.classList.remove("hidden"):a.classList.add("hidden");function u(s){const t="Description will be added sooner";return s.map(({_id:i,book_image:o,title:e,list_name:c,description:r,author:p,buy_links:l})=>`
       <li class="shopping-element">
         <img
-          src="${s}"
+          src="${o}"
           alt="${e}"
           class="book-image"
         />
@@ -17,7 +17,7 @@ import"./assets/dark-light-switcher-b7703df8.js";const n=document.querySelector(
               <li class="buy-link">
                 <a href="${l[0].url}" class="amazon-link book-link ">
                   <img
-                    src="../img/amazon@1x-min.png"
+                    src="/src/amazon@1x-min.png"
                     alt="Amazon"
                     width="32"
                     height="11"
