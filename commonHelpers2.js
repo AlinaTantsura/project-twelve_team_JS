358e1262844a94ce4ed5c29c95867739baba35d2
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-13 22:27:28 +0000

    Deploying to gh-pages from @ AlinaTantsura/project-twelve_team_JS@d2088f566287d54588c2660740d8410f341bd1e6 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,12 +1,12 @@
-import"./assets/dark-light-switcher-b7703df8.js";const n=document.querySelector(".books-list");document.querySelector(".delete-form");const a=document.querySelector(".no-books"),d=JSON.parse(localStorage.getItem("shoppingList"));n.insertAdjacentHTML("beforeend",u(d));const g=[...n.children];g.forEach(s=>{s.children[2].addEventListener("click",h)});function h(s){const t=s.currentTarget.dataset.bookId,i=JSON.parse(localStorage.getItem("shoppingList"))||[],e=[];i.filter(o=>{o._id!==t&&e.push(o)}),console.log(e),localStorage.setItem("shoppingList",JSON.stringify(e))}n.children.length===0?a.classList.remove("hidden"):a.classList.add("hidden");function u(s){const t="Description will be added sooner";return s.map(({_id:i,book_image:e,title:o,list_name:c,description:r,author:p,buy_links:l})=>`
+import"./assets/dark-light-switcher-b7703df8.js";const n=document.querySelector(".books-list");document.querySelector(".delete-form");const a=document.querySelector(".no-books"),d=JSON.parse(localStorage.getItem("shoppingList"));n.insertAdjacentHTML("beforeend",u(d));const g=[...n.children];g.forEach(o=>{o.children[2].addEventListener("click",h)});function h(o){const t=o.currentTarget.dataset.bookId,i=JSON.parse(localStorage.getItem("shoppingList"))||[],s=[];i.filter(e=>{e._id!==t&&s.push(e)}),console.log(s),localStorage.setItem("shoppingList",JSON.stringify(s))}n.children.length===0?a.classList.remove("hidden"):a.classList.add("hidden");function u(o){const t="Description will be added sooner";return o.map(({_id:i,book_image:s,title:e,list_name:c,description:r,author:p,buy_links:l})=>`
       <li class="shopping-element">
         <img
-          src="${e}"
-          alt="${o}"
+          src="${s}"
+          alt="${e}"
           class="book-image"
         />
         <div class="text-content">
-          <h2 class="book-title">${o}</h2>
+          <h2 class="book-title">${e}</h2>
           <p class="book-category">${c}</p>
           <p class="book-description">
             ${r||t}
@@ -15,9 +15,9 @@ import"./assets/dark-light-switcher-b7703df8.js";const n=document.querySelector(
             <p class="author-name">${p}</p>
             <ul class="buy-links">
               <li class="buy-link">
-                <a href="${l[0].url}" class="amazon-link">
+                <a href="${l[0].url}" class="amazon-link book-link ">
                   <img
-                    src="../img/amazon@1x-min.png"
+                    src="./img/amazon@1x-min.png"
                     alt="Amazon"
                     width="32"
                     height="11"
@@ -26,9 +26,9 @@ import"./assets/dark-light-switcher-b7703df8.js";const n=document.querySelector(
                 </a>
               </li>
               <li class="buy-link">
-                <a href="${l[1].url}" class="apple-book-link">
+                <a href="${l[1].url}" class="apple-book-link book-link ">
                   <img
-                    src="../img/appleBook@1x-min.png"
+                    src="./img/appleBook@1x-min.png"
                     alt="Apple Books"
                     width="16"
                     class="buy-apple"
