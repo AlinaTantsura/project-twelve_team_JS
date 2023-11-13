232c4bb5fd1889a0ae3786f47858a08b7fd4e5c1
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-13 13:24:56 +0200

    Summary

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -7,26 +7,24 @@ const currentPageBurger = window.location.pathname;
 
 // Function what open / close burger menu and blocked scroll
 function openCloseBurger() {
-    const isMenuClose = burgerMenu?.classList.contains('is-visible');
+    const isMenuClose = burgerMenu?.classList.contains('is-hidden');
     if(isMenuClose) {
         burgerMenu?.classList.remove('is-hidden');
-        document.body.style.position = 'fixed';
-        document.body.style.top = `-${window.scrollY}px`;
+        // document.body.style.position = 'fixed';
+        // document.body.style.top = `-${window.scrollY}px`;
 
         // For burger menu if open page shopping.html icon in navigation menu should be black
         if(currentPageBurger === '/shopping.html') {
-            // @ts-ignore
             cartBurgerIcon?.classList.add('active-burger-icon');
         }
     } else {
         burgerMenu?.classList.add('is-hidden');
-        const scrollY = document.body.style.top;
-        document.body.style.position = '';
-        document.body.style.top = '';
-        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+        // const scrollY = document.body.style.top;
+        // document.body.style.position = '';
+        // document.body.style.top = '';
+        // window.scrollTo(0, parseInt(scrollY || '0') * -1);
     }
 }
 
 openBurgerBtn?.addEventListener('click', openCloseBurger);
 closeBurgerBtn?.addEventListener('click', openCloseBurger);
-// ========================================================================================
