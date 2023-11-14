09498c106c781b4acdf0ad43a0e31c989cd7a260
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-14 07:50:12 +0000

    Deploying to gh-pages from @ AlinaTantsura/project-twelve_team_JS@f4caa66bcce1162012938e34614c271ef9ca53c1 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{i as d,a as h}from"./assets/dark-light-switcher-960ee4ab.js";const l=document.querySelector(".books-list");document.querySelector(".delete-form");const a=document.querySelector(".no-books"),g=JSON.parse(localStorage.getItem("shoppingList"));l.insertAdjacentHTML("beforeend",k(g));const u=[...l.children];u.forEach(s=>{s.children[2].addEventListener("click",m)});function m(s){const t=s.currentTarget.dataset.bookId,i=JSON.parse(localStorage.getItem("shoppingList"))||[],o=[];i.filter(e=>{e._id!==t&&o.push(e)}),console.log(o),localStorage.setItem("shoppingList",JSON.stringify(o))}l.children.length===0?a.classList.remove("hidden"):a.classList.add("hidden");function k(s){const t="Description will be added sooner";return s.map(({_id:i,book_image:o,title:e,list_name:c,description:r,author:p,buy_links:n})=>`
+import{a as d,b as h,c as g,d as u,i as m}from"./assets/dark-light-switcher-4a551f72.js";const l=document.querySelector(".books-list");document.querySelector(".delete-form");const n=document.querySelector(".no-books"),k=JSON.parse(localStorage.getItem("shoppingList"));l.insertAdjacentHTML("beforeend",f(k));const b=[...l.children];b.forEach(s=>{s.children[2].addEventListener("click",$)});function $(s){const t=s.currentTarget.dataset.bookId,i=JSON.parse(localStorage.getItem("shoppingList"))||[],o=[];i.filter(e=>{e._id!==t&&o.push(e)}),console.log(o),localStorage.setItem("shoppingList",JSON.stringify(o))}l.children.length===0?n.classList.remove("hidden"):n.classList.add("hidden");function f(s){const t="Description will be added sooner";return s.map(({_id:i,book_image:o,title:e,list_name:c,description:r,author:p,buy_links:a})=>`
       <li class="shopping-element">
         <img
           src="${o}"
@@ -15,9 +15,10 @@ import{i as d,a as h}from"./assets/dark-light-switcher-960ee4ab.js";const l=docu
             <p class="author-name">${p}</p>
             <ul class="buy-links">
               <li class="buy-link">
-                <a href="${n[0].url}" class="amazon-link book-link ">
+                <a href="${a[0].url}" class="amazon-link book-link ">
                   <img
                     src="${d}"
+                    srcset="${h} 2x"
                     alt="Amazon"
                     width="32"
                     height="11"
@@ -26,9 +27,10 @@ import{i as d,a as h}from"./assets/dark-light-switcher-960ee4ab.js";const l=docu
                 </a>
               </li>
               <li class="buy-link">
-                <a href="${n[1].url}" class="apple-book-link book-link ">
+                <a href="${a[1].url}" class="apple-book-link book-link ">
                   <img
-                    src="${h}"
+                    src="${g}"
+                    srcset="${u} 2x"
                     alt="Apple Books"
                     width="16"
                     class="buy-apple"
@@ -40,7 +42,7 @@ import{i as d,a as h}from"./assets/dark-light-switcher-960ee4ab.js";const l=docu
         </div>
         <button type="submit" class="delete-item" data-book-id="${i}">
           <svg class="remove-bin" width="16" height="16">
-            <use href="./img/InlineSprite.svg#trash-min"></use>
+            <use href="${m}#trash"></use>
           </svg>
         </button>
       </li>
