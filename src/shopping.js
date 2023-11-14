4c918ea91a3a12925d122d27e5cae2d14a632071
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-14 20:57:25 +0200

    Summary

--- src/shopping.js
+++ src/shopping.js
@@ -1,6 +1,6 @@
 import './js/shopping-list.js';
 import './js/support.js';
-import './js/sign-in.js';
+// import './js/sign-in.js';
 import './js/scroll-up.js';
 import './js/mobile-menu.js';
 import './js/header.js';
