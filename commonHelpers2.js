9a8e48faae8d07f555f73d8105bda98e8bdce206
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-14 16:22:57 +0000

    Deploying to gh-pages from @ AlinaTantsura/project-twelve_team_JS@163247b930e65c37a4791606e12ee41cbcb13f3a 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,24 +1,24 @@
-import{a as b,b as k,c as m,d as u,i as f}from"./assets/dark-light-switcher-2a87ab5d.js";const t=document.querySelector(".books-list"),c=document.querySelector(".no-books"),l=JSON.parse(localStorage.getItem("shoppingList"))??[];t.insertAdjacentHTML("beforeend",p(l));const r=l.length;let $=r;function L(s){location.reload();const i=s.currentTarget.dataset.bookId,n=JSON.parse(localStorage.getItem("shoppingList"))||[],e=[];n.forEach(o=>{o._id!==i&&e.push(o)}),localStorage.setItem("shoppingList",JSON.stringify(e))}r!==$&&t.insertAdjacentHTML("beforeend",p(l));const y=[...t.children];y.forEach(s=>{s.children[2].addEventListener("click",L)});t.children.length===0?c.classList.remove("hidden"):c.classList.add("hidden");function p(s){const i="Description will be added sooner";return s.map(({_id:n,book_image:e,title:o,list_name:d,description:h,author:g,buy_links:a})=>`
+import{a as m,b as u,c as h,d as g,i as k}from"./assets/dark-light-switcher-2a87ab5d.js";import{S as n}from"./assets/vendor-4589f348.js";const l=document.querySelector(".books-list"),c=document.querySelector(".no-books"),b=JSON.parse(localStorage.getItem("shoppingList"))??[];l.insertAdjacentHTML("beforeend",y(b));function f(o){const t=o.currentTarget.dataset.bookId,i=JSON.parse(localStorage.getItem("shoppingList"))||[],e=[];i.forEach(s=>{s._id!==t&&e.push(s)}),localStorage.setItem("shoppingList",JSON.stringify(e)),n.mixin({toast:!0,position:"top-end",showConfirmButton:!1,timer:1500,timerProgressBar:!0,didOpen:s=>{s.onmouseenter=n.stopTimer,s.onmouseleave=n.resumeTimer}}).fire({icon:"success",title:"Book was deleted successfully"}),localStorage.setItem("shoppingList",JSON.stringify(e)),setTimeout(()=>{location.reload()},1500)}const $=[...l.children];$.forEach(o=>{o.children[2].addEventListener("click",f)});l.children.length===0?c.classList.remove("hidden"):c.classList.add("hidden");function y(o){const t="Description will be added sooner";return o.map(({_id:i,book_image:e,title:a,list_name:s,description:p,author:d,buy_links:r})=>`
       <li class="shopping-element">
         <img
           src="${e}"
-          alt="${o}"
+          alt="${a}"
           class="book-image"
         />
         <div class="text-content">
-          <h2 class="book-title">${o}</h2>
-          <p class="book-category">${d}</p>
+          <h2 class="book-title">${a}</h2>
+          <p class="book-category">${s}</p>
           <p class="book-description">
-            ${h||i}
+            ${p||t}
           </p>
           <div class="author-buy">
-            <p class="author-name">${g}</p>
+            <p class="author-name">${d}</p>
             <ul class="buy-links">
               <li class="buy-link">
-                <a href="${a[0].url}" class="amazon-link book-link ">
+                <a href="${r[0].url}" class="amazon-link book-link ">
                   <img
-                    src="${b}"
-                    srcset="${k} 2x"
+                    src="${m}"
+                    srcset="${u} 2x"
                     alt="Amazon"
                     width="32"
                     height="11"
@@ -27,10 +27,10 @@ import{a as b,b as k,c as m,d as u,i as f}from"./assets/dark-light-switcher-2a87
                 </a>
               </li>
               <li class="buy-link">
-                <a href="${a[1].url}" class="apple-book-link book-link ">
+                <a href="${r[1].url}" class="apple-book-link book-link ">
                   <img
-                    src="${m}"
-                    srcset="${u} 2x"
+                    src="${h}"
+                    srcset="${g} 2x"
                     alt="Apple Books"
                     width="16"
                     class="buy-apple"
@@ -40,9 +40,9 @@ import{a as b,b as k,c as m,d as u,i as f}from"./assets/dark-light-switcher-2a87
             </ul>
           </div>
         </div>
-        <button type="submit" class="delete-item" data-book-id="${n}">
+        <button type="submit" class="delete-item" data-book-id="${i}">
           <svg class="remove-bin" width="16" height="16">
-            <use href="${f}#trash"></use>
+            <use href="${k}#trash"></use>
           </svg>
         </button>
       </li>
