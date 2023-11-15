6d590f169b874a238a70f3841cebc8691e219e85
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-15 10:11:02 +0200

    Update

--- src/js/header.js
+++ src/js/header.js
@@ -9,7 +9,8 @@ function setActivePage(className, currentPage) {
         // if (linkPath === currentPage || currentPage === '/' && linkPath === '/index.html') {
         if (currentPage.endsWith(linkPath) || currentPage.endsWith('/') && linkPath === '/index.html') {
             link.classList.add('active-header');
-            if (currentPage === '/shopping.html') {
+            // if (currentPage === '/shopping.html') {
+            if (currentPage.endsWith('/shopping.html')) {
                 iconBag?.classList.add('active-burger-icon');
             }
         } else {
