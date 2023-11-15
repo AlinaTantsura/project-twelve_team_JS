2801d1e9fbdfae350d9bb80163251789c0802b9b
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-15 09:08:30 +0200

    Summary

--- src/js/header.js
+++ src/js/header.js
@@ -5,9 +5,9 @@ function setActivePage(className, currentPage) {
     const navLinks = document.querySelectorAll(`.${className}`);
 
     navLinks.forEach(link => {
-        // @ts-ignore
         const linkPath = new URL(link.href).pathname;
-        if (linkPath === currentPage || currentPage === '/' && linkPath === '/index.html') {
+        // if (linkPath === currentPage || currentPage === '/' && linkPath === '/index.html') {
+        if (linkPath === currentPage || currentPage.endsWith('/') && linkPath === '/index.html') {
             link.classList.add('active-header');
             if (currentPage === '/shopping.html') {
                 iconBag?.classList.add('active-burger-icon');
