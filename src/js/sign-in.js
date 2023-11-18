0724096c2c0f9426e064f544ed0b3705343dc8d1
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2023-11-18 20:25:59 +0100

    update

--- src/js/sign-in.js
+++ src/js/sign-in.js
@@ -4,21 +4,13 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
   signInWithEmailAndPassword,
+  signOut,
 } from 'firebase/auth';
+import { getFirestore } from "firebase/firestore";
 import Swal from 'sweetalert2';
 
-// // import firebase from "firebase/compat/app";
-// import 'firebase/database';
-// import 'firebase/storage';
-// import 'firebase/messaging';
-
-// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
-// import { getDatabase } from "firebase/database";
-// // Посилання на об'єкт клієнта
-// var currentUser = "";
-// // Посилання на унікальний id клієнта в базі
-// var currentUserId = "";
 const auth = getAuth();
+const db = getFirestore(app);
 
 const openSignForm = document.querySelector('.js-open-signin');
 const closeSignForm = document.querySelector('.js-close-signin');
@@ -61,9 +53,12 @@ signInButtonLink?.addEventListener('click', event => {
 const userProfile = document.querySelector('.user-profile');
 const userNameText = document.querySelector('.header-user-name');
 const navigationHeader = document.querySelector('.header-nav');
+const logOutBtn = document.querySelector('.logout-button-overlay');
 
 formSignUp?.addEventListener('submit', signUpSubmit);
 formSignIn?.addEventListener('submit', signInSumbit);
+userProfile?.addEventListener('click', () => logOutBtn?.classList.add('show-logout'));
+logOutBtn?.addEventListener('click', onLogOut);
 
 // get new user data from Sign Up form
 function signUpSubmit(event) {
@@ -98,7 +93,7 @@ function whenUserSignUp(name, email, password) {
       toast: true,
       position: 'top-end',
       showConfirmButton: false,
-      timer: 3000,
+      timer: 1500,
       timerProgressBar: true,
       didOpen: toast => {
         toast.onmouseenter = Swal.stopTimer;
@@ -141,7 +136,7 @@ function whenUserSignIn(email, password) {
       toast: true,
       position: 'top-end',
       showConfirmButton: false,
-      timer: 3000,
+      timer: 1500,
       timerProgressBar: true,
       didOpen: toast => {
         toast.onmouseenter = Swal.stopTimer;
@@ -170,88 +165,42 @@ function whenUserSignIn(email, password) {
       });
   })
 }
+// ==================================================================================
+// FUNCTION WHEN USER LOG OUT ======================================================
 
-// //  Отримання посилання на базу данних
-// const database = getDatabase(app);
-
-// // Отримання посилання на відкриту сесію
-// const auth = getAuth();
-// console.log(auth);
-// // createUserWithEmailAndPassword(auth, email, password);
-// // createUserWithEmailAndPassword(auth, "anton@ukr.net", "asdfghj");
-// // GET Users info name
-// const txtName = document.getElementById('user-name');
-// // GET Users info e-mail
-// const txtEmail = document.getElementById('user-email');
-// // GET Users info password
-// const txtPassword = document.getElementById('user-password');
-// // Посилання на кнопку Login
-// const btnLogin    = document.getElementById('btnLogin');
-// // const btnSignUp   = document.getElementById('btnSignUp');
-// // const btnLogout   = document.getElementById('btnLogout');
-
-// //Add login event
-// btnLogin.addEventListener('click', e =>{
-//   "use strict";
-//     const email = txtEmail.value;
-//     const pass = txtPassword.value;
-//      // Sign in
-//     const promise = signInWithEmailAndPassword(auth, email, pass)
-//         .then((userCredential) => {
-//     // Signed in
-//             const user = userCredential.user;
-//     //  Посилання на об'єкт клієнта
-//             currentUser = user;
-//     //  Унікальний код клієнта в базі Firebase
-//             currentUserId = user.uid;
-//             alert(`Успіх. Код клієнта ${currentUserId}`)
-//             // console.log(user);
-//             // console.log(user.uid);
-//             // console.log(currentUserId);
-//     // Функція відключення видимості модального вікна
-//          toggleModal();
-
-//     // ...
-//   });
-
-//   promise.catch(e => alert(e.message));  //  console.log(e.message)
-
-// });
-
-// // Реєстрація нового користувача
-// async function registration(auth, email, password) {
-//     const data = await createUserWithEmailAndPassword(auth, email, password)
-//             .then((userCredential) => {
-//                 // Signed up
-//                 const user = userCredential.user;
-//                 // ...
-//             })
-//             .catch((error) => {
-//                 const errorCode = error.code;
-//                 const errorMessage = error.message;
-//                 alert(errorMessage);
-
-//             })
-//     };
-
-// // // Вхід в свій аккаунт
-// // async function login(auth, email, password) {
-// //            try {
-// //                const data = await signInWithEmailAndPassword(auth, email, password);
-// //                console.log(data.user.uid);
-// //            } catch (error) {
-// //                console.log(error.message);
-// //                throw error;
-// //            }
-// // };
-
-// // // Запис інформації користувача в базу
-// // async function newClient(newClinet) {
-// //            try {
-// //                const addClient = await firebase.database().ref('clients').push(newClinet);
-// //                console.log(addClient);
-// //            } catch (error) {
-// //                console.log(error.message);
-// //                throw error;
-// //            }
-// // };
+function onLogOut() {
+  signOut(auth).then(() => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true,
+      didOpen: toast => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      },
+    });
+    Toast.fire({
+      icon: 'success',
+      title: `Log Out was successfull`,
+    });
+  }).catch(() => {
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: toast => {
+        toast.onmouseenter = Swal.stopTimer;
+        toast.onmouseleave = Swal.resumeTimer;
+      },
+    });
+    Toast.fire({
+      icon: 'error',
+      title: `Ooops, Something go wrong!`,
+    });
+  });
+  location.href = './index.html';
+};
