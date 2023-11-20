81aad69c44828b7a9bc2e6644a04306226750e85
AlinaTantsura <133903876+AlinaTantsura@users.noreply.github.com>
2023-11-20 09:17:20 +0000

    Deploying to gh-pages from @ AlinaTantsura/project-twelve_team_JS@ba6742eab9f4c59259b603489464009aa48bd3b4 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,13 +1,13 @@
-import{a as w,b as P,c as A,d as I,i as E}from"./assets/dark-light-switcher-2616304d.js";import{S as c,P as T}from"./assets/vendor-13e289f0.js";const v=document.querySelector(".books-list"),k=document.querySelector(".no-books"),d=document.querySelector(".loader-backdrop");let S="shoppingList";const m=JSON.parse(localStorage.getItem(S))||[];p(m);function p(o){const e="Description will be added sooner",i=o.map(({_id:n,book_image:u,title:s,list_name:L,description:B,author:$,buy_links:h})=>`
+import{a as w,b as P,c as A,d as I,i as T,w as E}from"./assets/dark-light-switcher-03c03f45.js";import{S as c,P as q}from"./assets/vendor-dacd7886.js";const v=document.querySelector(".books-list"),k=document.querySelector(".no-books"),d=document.querySelector(".loader-backdrop");let S="shoppingList";const m=JSON.parse(localStorage.getItem(S))||[];p(m);function p(o){const e="Description will be added sooner",s=o.map(({_id:n,book_image:u,title:i,list_name:y,description:B,author:$,buy_links:h})=>`
       <li class="shopping-element" data-book-id="${n}" id="${n}">
         <img
           src="${u}"
-          alt="${s}"
+          alt="${i}"
           class="book-image"
         />
         <div class="text-content">
-          <h2 class="book-title">${s}</h2>
-          <p class="book-category">${L}</p>
+          <h2 class="book-title">${i}</h2>
+          <p class="book-category">${y}</p>
           <p class="book-description">
             ${B||e}
           </p>
@@ -42,9 +42,9 @@ import{a as w,b as P,c as A,d as I,i as E}from"./assets/dark-light-switcher-2616
         </div>
         <button type="submit" class="delete-item" data-book-id="${n}">
           <svg class="remove-bin" width="16" height="16">
-            <use href="${E}#trash"></use>
+            <use href="${T}#trash"></use>
           </svg>
         </button>
       </li>
-    `).join("");v.innerHTML=i,q()}function q(){document.querySelectorAll(".delete-item").forEach(e=>e.addEventListener("click",x))}function x(o){b();const e=o.target.closest("li").id;let i=JSON.parse(localStorage.getItem("shoppingList"))||[];i=i.filter(s=>s._id!==e),localStorage.setItem(S,JSON.stringify(i));const n=document.getElementById(e);n&&n.remove(),p(i),b(),c.mixin({toast:!0,position:"top-end",showConfirmButton:!1,timer:1500,timerProgressBar:!0,didOpen:s=>{s.onmouseenter=c.stopTimer,s.onmouseleave=c.resumeTimer}}).fire({icon:"success",title:"Book was deleted successfully"})}function b(){d==null||d.classList.toggle("is-hidden")}v.children.length===0||m.length===0||m===[]?k.classList.remove("hidden"):k.classList.add("hidden");let g=localStorage.getItem("shoppingList"),t=JSON.parse(g);const f=document.querySelector(".no-books");let y=2,l=4;window.innerWidth>=768&&(y=3,l=3);const O={totalItems:t.length,itemsPerPage:l,visiblePages:y},r=document.getElementById("tui-pagination-container"),a=new T(r,O);t.length<=3?r.classList.add("hidden"):r.classList.remove("hidden");a.on("beforeMove",o=>{const e=o.page;p(t.slice((e-1)*l,e*l)),z()});a.movePageTo(1);function z(){document.querySelectorAll(".delete-item").forEach(e=>e.addEventListener("click",J))}function J(){g=localStorage.getItem("shoppingList"),t=JSON.parse(g),t.length===0||t===[]?f.classList.remove("hidden"):f.classList.add("hidden");let o=a.getCurrentPage();a.reset(t.length),a.movePageTo(o),(t.length/l<=1||t.length<=3)&&r.classList.add("hidden")}
+    `).join("");v.innerHTML=s,x()}function x(){document.querySelectorAll(".delete-item").forEach(e=>e.addEventListener("click",O))}function O(o){b();const e=o.target.closest("li").id;let s=JSON.parse(localStorage.getItem("shoppingList"))||[];s=s.filter(i=>i._id!==e),localStorage.setItem(S,JSON.stringify(s)),E(s);const n=document.getElementById(e);n&&n.remove(),p(s),b(),c.mixin({toast:!0,position:"top-end",showConfirmButton:!1,timer:1500,timerProgressBar:!0,didOpen:i=>{i.onmouseenter=c.stopTimer,i.onmouseleave=c.resumeTimer}}).fire({icon:"success",title:"Book was deleted successfully"})}function b(){d==null||d.classList.toggle("is-hidden")}v.children.length===0||m.length===0||m===[]?k.classList.remove("hidden"):k.classList.add("hidden");let g=localStorage.getItem("shoppingList"),t=JSON.parse(g);const f=document.querySelector(".no-books");let L=2,l=4;window.innerWidth>=768&&(L=3,l=3);const z={totalItems:t.length,itemsPerPage:l,visiblePages:L},r=document.getElementById("tui-pagination-container"),a=new q(r,z);t.length<=3?r.classList.add("hidden"):r.classList.remove("hidden");a.on("beforeMove",o=>{const e=o.page;p(t.slice((e-1)*l,e*l)),J()});a.movePageTo(1);function J(){document.querySelectorAll(".delete-item").forEach(e=>e.addEventListener("click",N))}function N(){g=localStorage.getItem("shoppingList"),t=JSON.parse(g),t.length===0||t===[]?f.classList.remove("hidden"):f.classList.add("hidden");let o=a.getCurrentPage();a.reset(t.length),a.movePageTo(o),(t.length/l<=1||t.length<=3)&&r.classList.add("hidden")}
 //# sourceMappingURL=commonHelpers2.js.map
